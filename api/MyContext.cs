444c517818c1af2a84e079b937735781c2a55b35
Sami Jerrari <SamiJerrari547@gmail.com>
2024-10-16 11:14:06 +0200

    kleine verandering

--- api/MyContext.cs
+++ api/MyContext.cs
@@ -7,6 +7,7 @@ public class MyContext : DbContext
     public MyContext(DbContextOptions<MyContext> options) : base(options) {}
     public DbSet<Warehouses> Warehouses { get; set; }
     public DbSet<Contact> Contact { get; set; }
+    public DbSet<Locations> Locations {get;set;}
     // here comes the table names based on the class names (such as shipments, clients, items ect)
-    
+   
 }
