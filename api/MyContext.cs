7ceb115ebd78ef10c07839827864792f8714de54
Sami Jerrari <SamiJerrari547@gmail.com>
2024-10-19 15:04:19 +0200

    tussenstop(ik was aan het proberen met warehouseid link ik revert even de code terug)

--- api/MyContext.cs
+++ api/MyContext.cs
@@ -9,5 +9,12 @@ public class MyContext : DbContext
     public DbSet<Contact> Contact { get; set; }
     public DbSet<Locations> Locations {get;set;}
     // here comes the table names based on the class names (such as shipments, clients, items ect)
-   
+
+    //protected override void OnModelCreating(ModelBuilder modelBuilder)
+    //{
+        //modelBuilder.Entity<Locations>()
+            //.HasOne(l => l.Warehouse) // Specifies that Locations has one Warehouse
+            //.WithMany() // Specifies that Warehouse can have many Locations
+            //.HasForeignKey(l => l.WarehouseId); // Specifies the foreign key property
+    //}
 }
