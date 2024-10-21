a45209544b24c55ef875dc08b9bfd691c60f79f5
Ber-kan <bhgorur@gmail.com>
2024-10-21 13:57:41 +0200

    create client

---
+++ api/Models/Client.cs
@@ -0,0 +1,15 @@
+public class Client{
+    public required int Id {get; set;}
+    public required string Name {get; set;}
+    public required string Address {get; set;}
+    public required string City {get; set;}
+    public required string Zip {get; set;}
+    public required string Province {get; set;}
+    public required string Country {get; set;}
+    public required string Contact_name {get; set;}
+    public required string Contact_phone {get; set;}
+    public required string Contact_email {get; set;}
+    public required DateTime Created_at { get; set; }
+    public required DateTime Updated_at { get; set; }
+
+}
