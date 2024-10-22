bc025d32e14ad89f6f89d5574972ca00d4834ce6
Sami Jerrari <SamiJerrari547@gmail.com>
2024-10-22 13:07:55 +0200

    wat template adds zodat alles klaar staat

---
+++ api/Controllers/SuppliersController.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route(("/api/v1/locations"))]
+
+public class SuppliersController : ControllerBase
+{
+    
+}
--- api/Program.cs
+++ api/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build(); //waarom is dit weg in main?
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<MyContext>(x => x.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
@@ -9,8 +10,10 @@ builder.Services.AddTransient<LocationServices>();
 // Register services
 builder.Services.AddTransient<Warehouse>();
 builder.Services.AddTransient<Contact>();   
+builder.Services.AddTransient<Supplier>();
 builder.Services.AddTransient<WarehouseServices>(); 
 builder.Services.AddTransient<TransfersServices>();
+builder.Services.AddTransient<SuppliersServices>();
 
 app.MapControllers();
 app.Urls.Add("http://localhost:5000");
