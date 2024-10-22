c1f945f7b17f779b27903a4ef3abca29f79ed799
Sami Jerrari <SamiJerrari547@gmail.com>
2024-10-22 13:16:01 +0200

    om een of andere reden heeft de main geen ( var app = builder.Build()?

--- api/Program.cs
+++ api/Program.cs
@@ -1,7 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build(); //waarom is dit weg in main?
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<MyContext>(x => x.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
@@ -10,11 +9,10 @@ builder.Services.AddTransient<LocationServices>();
 // Register services
 builder.Services.AddTransient<Warehouse>();
 builder.Services.AddTransient<Contact>();   
-builder.Services.AddTransient<Supplier>();
 builder.Services.AddTransient<WarehouseServices>(); 
 builder.Services.AddTransient<TransfersServices>();
-builder.Services.AddTransient<SuppliersServices>();
 
+var app = builder.Build();
 app.MapControllers();
 app.Urls.Add("http://localhost:5000");
 app.MapGet("/", () => "Hello World!");
