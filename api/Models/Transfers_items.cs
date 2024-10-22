c7e0999d09bc98dc423aa0772aaabdce24bcdac6
Jason <jasonvuong@live.nl>
2024-10-22 19:44:41 +0200

    fixed transfer_ID showing in GET request

--- api/Models/Transfers_items.cs
+++ api/Models/Transfers_items.cs
@@ -1,5 +1,7 @@
+using System.Text.Json.Serialization;
 public class Transfers_item
 {
+    [JsonIgnore]
     public int TransferId { get; set; }
     public required string Item_Id { get; set; }
     public required int Amount { get; set; }
