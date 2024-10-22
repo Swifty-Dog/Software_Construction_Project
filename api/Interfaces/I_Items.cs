2a67899c968cd098baa4cc126a98992270bb816d
Ber-kan <bhgorur@gmail.com>
2024-10-22 21:20:48 +0200

    return type aangepast

--- api/Interfaces/I_Items.cs
+++ api/Interfaces/I_Items.cs
@@ -2,8 +2,8 @@ using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 public interface I_Items{
     public Task<IEnumerable<Item>> Get_Items();
-    public Task<Item> Get_Item_By_Id(int id);
+    public Task<Item> Get_Item_By_Id(string uid);
     public Task<Item> Add_Item(Item client);
-    public Task<Item> Update_Item(int id, Item client);
-    public Task<bool> Delete_Item(int id);
+    public Task<Item> Update_Item(string uid, Item item);
+    public Task<bool> Delete_Item(string uid);
     }
