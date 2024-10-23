e8aa066f62e05b36fdd1e6726b13b7b33851a4db
Sami Jerrari <SamiJerrari547@gmail.com>
2024-10-23 14:15:06 +0200

    revert rq

--- api/Interfaces/IOrdersinterface.cs
+++ api/Interfaces/IOrdersinterface.cs
@@ -1,8 +1,8 @@
 public interface IOrdersInterface
 {
-    Task<Order> Get(int id);
-    Task<IEnumerable<Order>> GetAll();
-    Task<Order> AddOrder(Order order);
-    Task<Order> UpdateOrder(int id, Order order);
+    Task<Orders> Get(int id);
+    Task<IEnumerable<Orders>> GetAll();
+    Task<Orders> AddOrder(Orders order);
+    Task<Orders> UpdateOrder(int id, Orders order);
     Task<bool> DeleteOrder(int id);
 }
