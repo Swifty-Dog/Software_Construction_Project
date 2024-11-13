d1dba8103cbcf9517db8512e489d539cdbb9a322
Ber-kan <bhgorur@gmail.com>
2024-11-13 13:59:35 +0100

    test bestand aangemaakt en start van class gemaakt

---
+++ Test/ClientsTest.cs
@@ -0,0 +1,11 @@
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Xunit;
+using System.Collections.Generic;
+
+public class ClientsTest{
+    
+}
