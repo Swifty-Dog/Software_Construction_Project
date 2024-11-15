ffcb1ec60516fa06c522d575a0ab3320105b238d
Jason <jasonvuong@live.nl>
2024-11-16 00:56:35 +0100

    added in-memory-database + rewrote tests

--- api/Models/Warehouses.cs
+++ api/Models/Warehouses.cs
@@ -5,6 +5,7 @@ public class Warehouse{
     public required string Address { get; set; }
     public required string Zip { get; set; }
     public required string City { get; set; }
+    public required string Province { get; set; }
     public required string Country { get; set; }
     public required Contact Contact {get; set; }
     public required DateTime Created_at { get; set; }
