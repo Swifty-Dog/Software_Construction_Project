a5230d9dc0cef5b5dd59146c3b97a190aebca227
Ber-kan <bhgorur@gmail.com>
2024-11-18 11:01:58 +0100

    invalid path test gemaakt

--- Test/ClientsTest.cs
+++ Test/ClientsTest.cs
@@ -66,4 +66,57 @@ public class ClientsTest
         Xunit.Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
     }
 
+    [Fact]
+    public async Task TestGetClientInvalidPath()
+    {
+        var response = await _client.GetAsync("Client/abc");
+        Xunit.Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+    }
+    
+/*
+    def test_get_empty_client_by_id(self):
+        response = self.client.get('clients/500000000')
+        client = response.json()
+        self.assertIsNone(client)
+        
+    def test_post_client(self):
+        self.base_url = 'http://localhost:3000/api/v1/'
+        response = self.client.post(self.base_url + "clients", json={
+            "id": 1,
+            "name": "Raymond Inc",
+            "address": "1296 Daniel Road Apt. 349",
+            "city": "Pierceview",
+            "zip_code": "28301",
+            "province": "Colorado",
+            "country": "United States",
+            "contact_name": "Bryan Clark",
+            "contact_phone": "242.732.3483x2573",
+            "contact_email": "test@",
+            "created_at": "2010-04-28 02:22:53",
+            "updated_at": "2022-02-09 20:22:35"
+        })
+        self.assertEqual(response.status_code, 201)
+    
+    def test_post_client_success_with_length(self):
+        response = self.client.get('clients')
+        old_client_length = len(response.json())
+        response = self.client.post('clients', json={
+            "id": 1,
+            "name": "Raymond Inc",
+            "address": "1296 Daniel Road Apt. 349",
+            "city": "Pierceview",
+            "zip_code": "28301",
+            "province": "Colorado",
+            "country": "United States",
+            "contact_name": "Bryan Clark",
+            "contact_phone": "242.732.3483x2573",
+            "contact_email": "test@",
+            "created_at": "2010-04-28 02:22:53",
+            "updated_at": "2022-02-09 20:22:35"
+        })
+        self.assertEqual(response.status_code, 201)
+        response = self.client.get('clients')
+        new_client_length = len(response.json())
+        self.assertTrue(new_client_length > old_client_length)
+*/
 }
