00c7a35dcb3dbb0976306b3526ebaa99bd5b615e
Ber-kan <bhgorur@gmail.com>
2024-11-19 21:21:09 +0100

    alle tests gemaakt en aangepast zodat het werkt met de pipeline. coverage command bij de readme gezet.

--- Test/ClientsTest.cs
+++ Test/ClientsTest.cs
@@ -1,139 +1,122 @@
-using System.Net;
-using System.Net.Http;
-using System.Net.Http.Json;
-using System.Threading.Tasks;
-using Newtonsoft.Json;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
 using Xunit;
-using System.Collections.Generic;
-
 public class ClientsTest
 {
-    private readonly HttpClient _client;
-    private readonly HttpClient _clientFail;
+    private readonly MyContext _context;
+    private readonly ClientController _controller;
 
     public ClientsTest()
     {
-        var apiKey = "a1b2c3d4e5";
+        var options = new DbContextOptionsBuilder<MyContext>()
+            .UseInMemoryDatabase(databaseName: "ClientTest")
+            .Options;
 
-        _client = new HttpClient
-        {
-            BaseAddress = new System.Uri("http://localhost:5000/api/v1/")
-            
-        };
-        _client.DefaultRequestHeaders.Add("Api-Key", apiKey);
+        _context = new MyContext(options);
+        SeedData();
 
-        _clientFail = new HttpClient
-        {
-            BaseAddress = new System.Uri("http://localhost:5000/api/v1/")
-        };
+        var service = new ClientServices(_context);
+        _controller = new ClientController(service);
     }
+    private void SeedData()
+    {
+        _context.Client.RemoveRange(_context.Client);
+        _context.SaveChanges();
 
-    // [Fact]
-    // public async Task TestClientAuthentication()
-    // {
+        var client = new Client
+        {
+            Id = 1,
+            Name = "Raymond Inc",
+            Address = "1296 Daniel Road Apt. 349",
+            City = "Pierceview",
+            Zip = "28301",
+            Province = "Colorado",
+            Country = "United States",
+            Contact_name = "Bryan Clark",
+            Contact_phone = "242.732.3483x2573",
+            Contact_email = "test@",
+            Created_at = DateTime.UtcNow,
+            Updated_at = DateTime.UtcNow
+        };
 
-    // }
+        _context.Client.Add(client);
+        _context.SaveChanges();
+    }
 
     [Fact]
     public async Task TestGetClients()
     {
-        var response = await _client.GetAsync("Clients");
-        Xunit.Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-
-        var content = await response.Content.ReadAsStringAsync();
-        Xunit.Assert.True(!string.IsNullOrEmpty(content), "Response content should not be empty");
+        var result = await _controller.Get_Clients();
+        var okResult = Xunit.Assert.IsType<OkObjectResult>(result);
+        var clients = Xunit.Assert.IsType<List<Client>>(okResult.Value);
+        Xunit.Assert.NotEmpty(clients);
     }
 
     [Fact]
     public async Task TestGetClientById()
     {
-        int clientId = 1;
-        var response = await _client.GetAsync($"Client/{clientId}");
-        Xunit.Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-
-        var content = await response.Content.ReadAsStringAsync();
-        var client = JsonConvert.DeserializeObject<Client>(content);
-
-        Xunit.Assert.Equal(clientId, client.Id);
+        var result = await _controller.Get_Client_By_Id(1);
+        var okResult = Xunit.Assert.IsType<OkObjectResult>(result);
+        var client = Xunit.Assert.IsType<Client>(okResult.Value);
+        Xunit.Assert.Equal("Raymond Inc", client.Name);
+        Xunit.Assert.Equal(1, client.Id);
     }
 
     [Fact]
     public async Task TestGetNonexistentClient()
     {
-        int clientId = 1000000;
-        var response = await _client.GetAsync($"Client/{clientId}");
-
-        Xunit.Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
-    }
-
-    [Fact]
-    public async Task TestGetClientInvalidPath()
-    {
-        var response = await _client.GetAsync("Client/abc");
-        Xunit.Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        var result = await _controller.Get_Client_By_Id(9999);
+        Xunit.Assert.IsType<NotFoundObjectResult>(result);
     }
     
-    [Fact]
-    public async Task TestPostClient() // nog bezig
-    {
-        var initialResponse = await _client.GetAsync("Client");
-        var initialContent = await initialResponse.Content.ReadAsStringAsync();
-        var initialClient = JsonConvert.DeserializeObject<List<Client>>(initialContent);
-        var oldLength = initialClient.Count;
-
-        var clientData = new
+     [Fact]
+     public async Task TestPostClient()
+     {
+        var newclient = new Client
         {
-            id = 123456789,
-            name = "Raymond Inc",
-            address = "1296 Daniel Road Apt. 349",
-            city = "Pierceview",
-            zip_code = "28301",
-            province = "Colorado",
-            country = "United States",
-            contact_name = "Bryan Clark",
-            contact_phone = "242.732.3483x2573",
-            contact_email = "test@",
-            created_at = "2010-04-28 02:22:53",
-            updated_at = "2022-02-09 20:22:35"
+            Id = 123,
+            Name = "new inc",
+            Address = "new addres",
+            City = "new city",
+            Zip = "111",
+            Province = "new province",
+            Country = "new country",
+            Contact_name = "new name",
+            Contact_phone = "06 12345678",
+            Contact_email = "test@",
+            Created_at = DateTime.UtcNow,
+            Updated_at = DateTime.UtcNow
+        };
+        var result = await _controller.Add_Client(newclient);
+        var okResult = Xunit.Assert.IsType<OkObjectResult>(result);
+        var client = Xunit.Assert.IsType<Client>(okResult.Value);
+        Xunit.Assert.Equal(123, client.Id);
+        Xunit.Assert.Equal("new inc", client.Name);
+     }
+    
+    [Fact]
+    public async Task TestPutClient()
+    {
+        var updatedClient = new Client
+        {
+            Id = 1,
+            Name = "changed inc",
+            Address = "changed addres",
+            City = "changed city",
+            Zip = "999",
+            Province = "changed province",
+            Country = "changed country",
+            Contact_name = "new name",
+            Contact_phone = "06 12345678",
+            Contact_email = "test@",
+            Created_at = DateTime.UtcNow,
+            Updated_at = DateTime.UtcNow
         };
 
-        var postResponse = await _client.PostAsJsonAsync("Client", clientData);
-
-        Xunit.Assert.Equal(HttpStatusCode.OK, postResponse.StatusCode);
-
-        var newResponse = await _client.GetAsync("Client");
-        var newContent = await newResponse.Content.ReadAsStringAsync();
-        var newClient = JsonConvert.DeserializeObject<List<Client>>(newContent);
-        var newLength = newClient.Count;
-
-        Xunit.Assert.True(newLength > oldLength);
-    }
-/*
-    def test_get_empty_client_by_id(self):
-        response = self.client.get('clients/500000000')
-        client = response.json()
-        self.assertIsNone(client)
-        
-    def test_post_client_success_with_length(self):
-        response = self.client.get('clients')
-        old_client_length = len(response.json())
-        response = self.client.post('clients', json={
-            "id": 1,
-            "name": "Raymond Inc",
-            "address": "1296 Daniel Road Apt. 349",
-            "city": "Pierceview",
-            "zip_code": "28301",
-            "province": "Colorado",
-            "country": "United States",
-            "contact_name": "Bryan Clark",
-            "contact_phone": "242.732.3483x2573",
-            "contact_email": "test@",
-            "created_at": "2010-04-28 02:22:53",
-            "updated_at": "2022-02-09 20:22:35"
-        })
-        self.assertEqual(response.status_code, 201)
-        response = self.client.get('clients')
-        new_client_length = len(response.json())
-        self.assertTrue(new_client_length > old_client_length)
-*/
+        var result = await _controller.Update_Client(1, updatedClient);
+        var okResult = Xunit.Assert.IsType<OkObjectResult>(result);
+        var client = Xunit.Assert.IsType<Client>(okResult.Value);
+        Xunit.Assert.Equal("changed inc", client.Name);
+        Xunit.Assert.Equal("999", client.Zip);
+    } 
 }
