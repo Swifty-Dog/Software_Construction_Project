2e49c9cdb4e5a97cfb67e1d29f88d2c24b55a3c3
Ber-kan <bhgorur@gmail.com>
2024-11-24 11:15:29 +0100

    pascalcase change

--- api/Models/ItemLine.cs
+++ api/Models/ItemLine.cs
@@ -2,6 +2,6 @@ public class Itemline {
     public required int Id { get; set; }
     public required string Name { get; set; }
     public required string Description { get; set; }
-    public required DateTime Created_at { get; set; }
-    public required DateTime Updated_at { get; set; }
+    public required DateTime CreatedAt { get; set; }
+    public required DateTime UpdatedAt { get; set; }
 }
