e792aaef5507e14cfe447359b96f1046f27629e2
Sami Jerrari <SamiJerrari547@gmail.com>
2024-12-03 22:31:13 +0100

    aanmaak warehousesunittest.cs

---
+++ Test/WarehousesUnitTest.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+using System.Threading.Tasks;
+using System.Linq;
+using System;
+
+public class WarehouseServicesTest
+{
+    private readonly MyContext _context;
+    private readonly WarehouseServices _service;
+}
