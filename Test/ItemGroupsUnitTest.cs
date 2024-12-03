d8c0822adfc219bf3d987b5225d35c33d125b8b9
Sami Jerrari <SamiJerrari547@gmail.com>
2024-12-03 23:42:15 +0100

    setup itemgroupsunittest

---
+++ Test/ItemGroupsUnitTest.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+using System.Threading.Tasks;
+using System.Linq;
+using System;
+
+public class ItemGroupsUnitTest
+{
+    private readonly MyContext _context;
+    private readonly ItemGroupController _controller;
+}
