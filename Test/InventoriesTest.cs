0504eca2b5ad418b4d4d3a1df9e9281f78dc818f
Ber-kan <bhgorur@gmail.com>
2024-12-06 19:41:04 +0100

    pascal aanpassingen

--- Test/InventoriesTest.cs
+++ Test/InventoriesTest.cs
@@ -28,23 +28,23 @@ public class InventoriesTest
 
         var inventory = new Inventory
         {
-            Id = 100,
-            Item_id = "P000001",
-            Description = "Face-to-face clear-thinking complexity",
-            Item_reference = "sjQ23408K",
-            Locations = new List<Inventories_locations>
+            id = 100,
+            itemId = "P000001",
+            description = "Face-to-face clear-thinking complexity",
+            itemReference = "sjQ23408K",
+            locations = new List<InventoriesLocations>
             {
-                new Inventories_locations { LocationId = 3211 },
-                new Inventories_locations { LocationId = 24700 },
-                new Inventories_locations { LocationId = 14123 }
+                new InventoriesLocations { locationId = 3211 },
+                new InventoriesLocations { locationId = 24700 },
+                new InventoriesLocations { locationId = 14123 }
             },
-            Total_on_hand = 262,
-            Total_expected = 0,
-            Total_ordered = 80,
-            Total_allocated = 41,
-            Total_available = 141,
-            Created_at = DateTime.Parse("2015-02-19 16:08:24"),
-            Updated_at = DateTime.Parse("2015-09-26 06:37:56")
+            totalOnHand = 262,
+            totalExpected = 0,
+            totalOrdered = 80,
+            totalAllocated = 41,
+            totalAvailable = 141,
+            createdAt = DateTime.Parse("2015-02-19 16:08:24"),
+            updatedAt = DateTime.Parse("2015-09-26 06:37:56")
         };
 
         _context.Inventories.Add(inventory);
@@ -52,37 +52,37 @@ public class InventoriesTest
     }
 
     [Fact]
-    public async Task Test_Get_Inventories()
+    public async Task TestGetInventories()
     {
-        var result = await _controller.Get_Inventories();
+        var result = await _controller.GetInventories();
         var okResult = Xunit.Assert.IsType<OkObjectResult>(result);
         var inventories = Xunit.Assert.IsType<List<Inventory>>(okResult.Value);
         Xunit.Assert.NotEmpty(inventories);
     }
 
     [Fact]
-    public async Task Test_Get_Inventory_By_Id()
+    public async Task TestGetInventoryById()
     {
-        var result = await _controller.Get_Inventory_By_Id(100);
+        var result = await _controller.GetInventoryById(100);
         var okResult = Xunit.Assert.IsType<OkObjectResult>(result);
         var inventory = Xunit.Assert.IsType<Inventory>(okResult.Value);
-        Xunit.Assert.Equal("P000001", inventory.Item_id);
-        Xunit.Assert.Equal(100, inventory.Id);
+        Xunit.Assert.Equal("P000001", inventory.itemId);
+        Xunit.Assert.Equal(100, inventory.id);
     }
 
     [Fact]
-    public async Task Test_Get_Non_Existent_Inventory()
+    public async Task TestGet_Non_Existent_Inventory()
     {
-        var result = await _controller.Get_Inventory_By_Id(9999);
+        var result = await _controller.GetInventoryById(9999);
         Xunit.Assert.IsType<NotFoundObjectResult>(result);
     }
 
     [Fact]
-    public async Task Test_Get_Inventory_Locations()
+    public async Task TestGetInventoryLocations()
     {
-        var result = await _controller.Get_Inventory_Locations(100);
+        var result = await _controller.GetInventoryLocations(100);
         var okResult = Xunit.Assert.IsType<OkObjectResult>(result);
-        var locations = Xunit.Assert.IsType<List<Inventories_locations>>(okResult.Value);
+        var locations = Xunit.Assert.IsType<List<InventoriesLocations>>(okResult.Value);
         Xunit.Assert.Equal(3, locations.Count);
     }
 
@@ -91,22 +91,22 @@ public class InventoriesTest
     {
         var newInventory = new Inventory
         {
-            Id = 101,
-            Item_id = "P000002",
-            Description = "Focused transitional alliance",
-            Item_reference = "nyg48736S",
-            Locations = new List<Inventories_locations>
+            id = 101,
+            itemId = "P000002",
+            description = "Focused transitional alliance",
+            itemReference = "nyg48736S",
+            locations = new List<InventoriesLocations>
             {
-                new Inventories_locations { LocationId = 19800 },
-                new Inventories_locations { LocationId = 23653 }
+                new InventoriesLocations { locationId = 19800 },
+                new InventoriesLocations { locationId = 23653 }
             },
-            Total_on_hand = 194,
-            Total_expected = 0,
-            Total_ordered = 139,
-            Total_allocated = 0,
-            Total_available = 55,
-            Created_at = DateTime.UtcNow,
-            Updated_at = DateTime.UtcNow
+            totalOnHand = 194,
+            totalExpected = 0,
+            totalOrdered = 139,
+            totalAllocated = 0,
+            totalAvailable = 55,
+            createdAt = DateTime.UtcNow,
+            updatedAt = DateTime.UtcNow
         };
 
         var result = await _controller.AddInventory(newInventory);
@@ -119,21 +119,21 @@ public class InventoriesTest
     {
         var existingInventory = new Inventory
         {
-            Id = 100,
-            Item_id = "P000001",
-            Description = "Duplicate Inventory",
-            Item_reference = "duplicateRef",
-            Locations = new List<Inventories_locations>
+            id = 100,
+            itemId = "P000001",
+            description = "Duplicate Inventory",
+            itemReference = "duplicateRef",
+            locations = new List<InventoriesLocations>
             {
-                new Inventories_locations { LocationId = 12345 }
+                new InventoriesLocations { locationId = 12345 }
             },
-            Total_on_hand = 100,
-            Total_expected = 0,
-            Total_ordered = 50,
-            Total_allocated = 25,
-            Total_available = 25,
-            Created_at = DateTime.UtcNow,
-            Updated_at = DateTime.UtcNow
+            totalOnHand = 100,
+            totalExpected = 0,
+            totalOrdered = 50,
+            totalAllocated = 25,
+            totalAvailable = 25,
+            createdAt = DateTime.UtcNow,
+            updatedAt = DateTime.UtcNow
         };
 
         var result = await _controller.AddInventory(existingInventory);
@@ -144,17 +144,17 @@ public class InventoriesTest
     [Fact]
     public async Task TestDeleteInventory()
     {
-        var result = await _controller.Delete_Inventory(100);
+        var result = await _controller.DeleteInventory(100);
         Xunit.Assert.IsType<OkObjectResult>(result);
 
-        var getResult = await _controller.Get_Inventory_By_Id(100);
+        var getResult = await _controller.GetInventoryById(100);
         Xunit.Assert.IsType<NotFoundObjectResult>(getResult);
     }
 
     [Fact]
     public async Task TestDeleteNonExistentInventory()
     {
-        var result = await _controller.Delete_Inventory(9999);
+        var result = await _controller.DeleteInventory(9999);
         var badRequestResult = Xunit.Assert.IsType<BadRequestObjectResult>(result);
         Xunit.Assert.Equal("Inventory could not be deleted or does not exist.", badRequestResult.Value);
     }
