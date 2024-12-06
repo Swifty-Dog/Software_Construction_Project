6fcd7fafd42c999f2a9fe59b632d59052e9ff18b
Ber-kan <bhgorur@gmail.com>
2024-12-06 19:39:16 +0100

    pascal aanpassingen

--- api/Interfaces/IInventories.cs
+++ api/Interfaces/IInventories.cs
@@ -1,7 +1,7 @@
 public interface IInventories{
     public Task<IEnumerable<Inventory>> GetInventories();
     public Task<Inventory> GetInventoryById(int id);
-    public Task<List<Inventories_locations>> GetInventoryLocations(int id);
+    public Task<List<InventoriesLocations>> GetInventoryLocations(int id);
     public Task<Inventory> AddInventory(Inventory inventory);
     public Task<Inventory> UpdateInventory(int id, Inventory inventory);
     public Task<bool> DeleteInventory(int id);
