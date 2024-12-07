9104c5fe25a6f05a5e03fe6b8a218d619cdc7328
Jason <jasonvuong@live.nl>
2024-12-07 02:22:03 +0100

    done with styling of warehouses files

--- api/Interfaces/IWarehouses.cs
+++ api/Interfaces/IWarehouses.cs
@@ -1,8 +1,8 @@
-public interface IWarehouse{
-    public Task<IEnumerable<Warehouse>> Get_Warehouses();
-    public Task<Warehouse> Get_Warehouse_By_Id(int id);
-    public Task<Warehouse> Add_Warehouse(Warehouse warehouse);
-    public Task<Warehouse> Update_Warehouse(int id, Warehouse warehouse);
-    public Task<bool> Delete_Warehouse(int id);
- 
+public interface IWarehouse
+{
+    Task<IEnumerable<Warehouse>> GetWarehouses();
+    Task<Warehouse> GetWarehouseById(int id);
+    Task<Warehouse> AddWarehouse(Warehouse warehouse);
+    Task<Warehouse> UpdateWarehouse(int id, Warehouse warehouse);
+    Task<bool> DeleteWarehouse(int id);
 }
