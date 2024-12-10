c896e5ea3b666e224fee28da79c8ecacd5dfdc4b
Ber-kan <bhgorur@gmail.com>
2024-12-10 20:14:32 +0100

    pascal changes: class en method naam aangepast

--- api/Interfaces/IItemLine.cs
+++ api/Interfaces/IItemLine.cs
@@ -2,7 +2,7 @@ public interface IItemLine
 {
     public Task<IEnumerable<ItemLine>> GetItemLine();
     public Task<ItemLine> GetItemLineById(int id);
-    // public Task<Item_group> Get_Item_group_Item_Id(int id);
+    // public Task<ItemGroup> GetItemGroupItemId(int id);
     
     public Task<ItemLine> UpdateItemLine(ItemLine itemLine, int id);
     public Task<bool> DeleteItemLine(int id);
