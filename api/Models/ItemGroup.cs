d51684bf62271acb60477bbe30ce0b12cc93a148
Ber-kan <bhgorur@gmail.com>
2024-12-10 20:27:03 +0100

    pascal changes: property namen

--- api/Models/ItemGroup.cs
+++ api/Models/ItemGroup.cs
@@ -3,6 +3,6 @@ public class ItemGroup
     public required int Id { get; set; }
     public required string Name { get; set; }
     public required string Description { get; set; }
-    public required DateTime Created_at { get; set; }
-    public required DateTime Updated_at { get; set; }
+    public required DateTime CreatedAt { get; set; }
+    public required DateTime UpdatedAt { get; set; }
 }
