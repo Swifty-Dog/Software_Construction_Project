80216c358ad317f0346f88120879bd26bf87400d
Ber-kan <bhgorur@gmail.com>
2024-12-10 20:01:50 +0100

    add method stond er niet bij dus die geplaatst

--- api/Interfaces/IItemGroup.cs
+++ api/Interfaces/IItemGroup.cs
@@ -3,6 +3,7 @@ public interface IItemGroup
     public Task<IEnumerable<ItemGroup>> GetItemGroups();
     public Task<ItemGroup> GetItemGroupById(int id);
     // public Task<ItemGroup> GetItemGroupItemId(int id);
+    public Task<ItemGroup> AddItemGroup(ItemGroup itemGroup);
     public Task<ItemGroup> UpdateItemGroup(ItemGroup itemGroup);
     public Task<bool> DeleteItemGroup(int id);
 }
