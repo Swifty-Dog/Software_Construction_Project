1badf78997d74646e2d6b455582eb67bb0fb3ade
aym1279 <1040029@hr.nl>
2024-12-10 05:17:50 +0100

    fixed renaming for suppliers model and discovered error in data

--- api/Models/Suppliers.cs
+++ api/Models/Suppliers.cs
@@ -4,14 +4,14 @@ public class Supplier
     public required string Code { get; set; }
     public required string Name { get; set; }
     public required string Address { get; set; }
-    public required string Address_extra { get; set; }
-    public required string Zip_code { get; set; }
+    public required string AddressExtra { get; set; }
+    public required string City {get; set;}
+    public required string ZipCode { get; set; }
     public required string Province { get; set; }
     public required string Country { get; set; }
-    public required string Contact_name {get; set;}
+    public required string ContactName {get; set;}
     public required string Phonenumber {get; set;}
-    public required string Email {get; set;}
     public required string Reference {get; set; }
-    public required DateTime Created_at { get; set; }
-    public required DateTime Updated_at { get; set; }
+    public required DateTime CreatedAt { get; set; }
+    public required DateTime UpdatedAt { get; set; }
 }
