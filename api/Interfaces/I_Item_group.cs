744c29e88d8cdad2834a8f69111d589578c99a67
Ber-kan <bhgorur@gmail.com>
2024-12-10 19:52:41 +0100

    pascal changes: interface en methods namen

--- api/Interfaces/I_Item_group.cs
+++
@@ -1,8 +0,0 @@
-public interface I_Item_group
-{
-    public Task<IEnumerable<Item_group>> Get_Item_groups();
-    public Task<Item_group> Get_Item_group_By_Id(int id);
-    // public Task<Item_group> Get_Item_group_Item_Id(int id);
-    public Task<Item_group> Update_Item_group(Item_group item_group);
-    public Task<bool> Delete_Item_group(int id);
-}
