280f723126371887d077e30b13764fdd8c7fcb14
Ber-kan <bhgorur@gmail.com>
2025-01-08 15:33:22 +0100

    Item properties nullable gemaakt

--- api/Models/Item.cs
+++ api/Models/Item.cs
@@ -6,13 +6,13 @@ public class Item {
     public required string UpcCode { get; set; }
     public required string ModelNumber { get; set; }
     public required string CommodityCode { get; set; }
-    public required int ItemLine { get; set; }  
-    public required int ItemGroup { get; set; }
-    public required int ItemType { get; set; }
+    public int? ItemLine { get; set; } = null;
+    public int? ItemGroup { get; set; } = null;
+    public int? ItemType { get; set; } = null;
     public required int UnitPurchaseQuantity { get; set; }
     public required int UnitOrderQuantity { get; set; }
     public required int PackOrderQuantity { get; set; }
-    public required int SupplierId { get; set; }
+    public int? SupplierId { get; set; }
     public required string SupplierCode { get; set; }
     public required string SupplierPartNumber { get; set; }
     public required DateTime CreatedAt { get; set; }
