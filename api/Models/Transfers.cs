8227ddabad6e6f50461110d596549af98167b5d3
aym1279 <1040029@hr.nl>
2025-01-15 11:38:12 +0100

    transfer models updated

--- api/Models/Transfers.cs
+++ api/Models/Transfers.cs
@@ -1,13 +1,31 @@
 using System.ComponentModel.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
 
 public class Transfer
 {
+    [JsonPropertyName("id")]
     public required int Id { get; set; }
+
+    [JsonPropertyName("reference")]
     public required string Reference { get; set; }
+
+    [JsonPropertyName("transfer_from")]
     public required int? TransferFrom { get; set; }
+
+    [JsonPropertyName("transfer_to")]
     public required int TransferTo { get; set; }
+
+    [JsonPropertyName("transfer_status")]
     public required string TransferStatus { get; set; }
+
+    [JsonPropertyName("created_at")]
     public required DateTime CreatedAt { get; set; }
+
+    [JsonPropertyName("updated_at")]
     public required DateTime UpdatedAt { get; set; }
-    public required List<TransfersItem> Items { get; set; } = new();
+
+    [JsonPropertyName("items")]
+    public required List<TransfersItem> Items { get; set; } = new List<TransfersItem>();
 }
--- api/Models/TransfersItems.cs
+++ api/Models/TransfersItems.cs
@@ -1,8 +1,13 @@
 using System.Text.Json.Serialization;
+
 public class TransfersItem
 {
     [JsonIgnore]
     public int TransferId { get; set; }
+
+    [JsonPropertyName("item_id")]
     public required string ItemId { get; set; }
+
+    [JsonPropertyName("amount")]
     public required int Amount { get; set; }
 }
