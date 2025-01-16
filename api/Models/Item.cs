fb1e062f9804cd18287d7efc9027b747e1b20c67
aym1279 <1040029@hr.nl>
2025-01-16 17:55:02 +0100

    updated items model

--- api/Models/Item.cs
+++ api/Models/Item.cs
@@ -1,20 +1,58 @@
-public class Item {
+using System.Text.Json.Serialization;
+
+public class Item
+{
+    [JsonPropertyName("uid")]
     public required string Uid { get; set; }
+
+    [JsonPropertyName("code")]
     public required string Code { get; set; }
+
+    [JsonPropertyName("description")]
     public required string Description { get; set; }
+
+    [JsonPropertyName("short_description")]
     public required string ShortDescription { get; set; }
+
+    [JsonPropertyName("upc_code")]
     public required string UpcCode { get; set; }
+
+    [JsonPropertyName("model_number")]
     public required string ModelNumber { get; set; }
+
+    [JsonPropertyName("commodity_code")]
     public required string CommodityCode { get; set; }
-    public int? ItemLine { get; set; } = null;
-    public int? ItemGroup { get; set; } = null;
-    public int? ItemType { get; set; } = null;
+
+    [JsonPropertyName("item_line")]
+    public required int? ItemLine { get; set; } = null;
+
+    [JsonPropertyName("item_group")]
+    public required int? ItemGroup { get; set; } = null;
+
+    [JsonPropertyName("item_type")]
+    public required int? ItemType { get; set; } = null;
+
+    [JsonPropertyName("unit_purchase_quantity")]
     public required int UnitPurchaseQuantity { get; set; }
+
+    [JsonPropertyName("unit_order_quantity")]
     public required int UnitOrderQuantity { get; set; }
+
+    [JsonPropertyName("pack_order_quantity")]
     public required int PackOrderQuantity { get; set; }
-    public int? SupplierId { get; set; }
+
+    [JsonPropertyName("supplier_id")]
+    public required int? SupplierId { get; set; }
+
+    [JsonPropertyName("supplier_code")]
     public required string SupplierCode { get; set; }
+
+    [JsonPropertyName("supplier_part_number")]
     public required string SupplierPartNumber { get; set; }
+
+    [JsonPropertyName("created_at")]
     public required DateTime CreatedAt { get; set; }
+
+    [JsonPropertyName("updated_at")]
     public required DateTime UpdatedAt { get; set; }
 }
