e067b53701dbc985a5c9aa81826255683079f5e9
aym1279 <1040029@hr.nl>
2025-01-16 18:22:04 +0100

    updated item line model

--- api/Models/ItemLine.cs
+++ api/Models/ItemLine.cs
@@ -1,7 +1,19 @@
-public class ItemLine {
+using System.Text.Json.Serialization;
+
+public class ItemLine
+{
+    [JsonPropertyName("id")]
     public required int Id { get; set; }
+    
+    [JsonPropertyName("name")]
     public required string Name { get; set; }
+    
+    [JsonPropertyName("description")]
     public required string Description { get; set; }
+    
+    [JsonPropertyName("created_at")]
     public required DateTime CreatedAt { get; set; }
+    
+    [JsonPropertyName("updated_at")]
     public required DateTime UpdatedAt { get; set; }
 }
